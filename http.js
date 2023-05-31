0a4623b76fdc8a140457466e40869cb6662b6641
Riley <riley.ledrew@keyin.com>
2023-05-31 15:26:37 -0230

    finished initial setup of server with to-be routes

--- http.js
+++ http.js
@@ -2,7 +2,26 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url);
+    switch (req.url) {
+      case "/about":
+        res.write("WHAT'S THIS ALL ABOUT?????");
+        break;
+      case "/contact":
+        res.write("Do not contact me");
+        break;
+      case "/products":
+        res.write("Khajit has wares if you has coin");
+        break;
+      case "/subscribe":
+        res.write("snubsnirb");
+        break;
+      case "/":
+        res.write("you sneaky you");
+        break;
+      case "/woah":
+        res.write("Far out man");
+        break;
+    }
     res.end();
   })
   .listen(8080);
