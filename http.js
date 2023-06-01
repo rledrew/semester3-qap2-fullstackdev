9ddff7319953bca18d5f0f1f73594f4bfd54ec36
Riley <riley.ledrew@keyin.com>
2023-05-31 23:46:18 -0230

    setup emitters

--- http.js
+++ http.js
@@ -1,17 +1,26 @@
 var http = require("http");
 var fs = require("fs");
+var EventEmitter = require("events");
+
+var myEmitter = new EventEmitter();
+
 http
   .createServer(function (req, res) {
+    myEmitter.on("writeToConsole", (message) => {
+      console.log(message);
+    });
     switch (req.url) {
       case "/about":
         fs.readFile("views/about.html", (err, data) => {
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("File not found");
+            myEmitter.emit("writeToConsole", "No file found");
             return;
           } else {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write(data);
+            myEmitter.emit("writeToConsole", "About page was visited");
           }
           res.end();
         });
@@ -21,10 +30,12 @@ http
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("File not found");
+            myEmitter.emit("writeToConsole", "No file found");
             return;
           } else {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write(data);
+            myEmitter.emit("writeToConsole", "Contact page was visited");
           }
           res.end();
         });
@@ -34,10 +45,12 @@ http
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("File not found");
+            myEmitter.emit("writeToConsole", "No file found");
             return;
           } else {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write(data);
+            myEmitter.emit("writeToConsole", "Products page was visited");
           }
           res.end();
         });
@@ -47,10 +60,12 @@ http
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("File not found");
+            myEmitter.emit("writeToConsole", "No file found");
             return;
           } else {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write(data);
+            myEmitter.emit("writeToConsole", "Subscribe page was visited");
           }
           res.end();
         });
@@ -60,6 +75,7 @@ http
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("File not found");
+            myEmitter.emit("writeToConsole", "No file found");
             return;
           } else {
             res.writeHead(200, { "Content-Type": "text/html" });
@@ -73,10 +89,12 @@ http
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("File not found");
+            myEmitter.emit("writeToConsole", "No file found");
             return;
           } else {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write(data);
+            myEmitter.emit("writeToConsole", "Woah page was visited");
           }
           res.end();
         });
